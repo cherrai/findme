0d3c1757ede5eeffeccc24fe8174a6b9777632c0
yuki <nahcikuy@gmail.com>
2023-08-21 12:29:11 +0800

    real initial commit

---
+++ index.ts
@@ -0,0 +1,47 @@
+import {
+  GeoInfo,
+  GeoInfoResponse,
+  GeoInfoSuccess,
+  ProxySettings,
+} from './typing.js';
+
+const IS_IN_BROWSER = globalThis.window ? true : false;
+
+function getGeoInfoFromResponse(response: GeoInfoSuccess) {
+  return Object.fromEntries(
+    Object.entries(response).filter(
+      ([key, _]) => !['status', 'message'].includes(key)
+    )
+  ) as GeoInfo;
+}
+
+/** ProxySettings won't work in browser side. */
+export async function getGeoInfo(
+  ip: string = '',
+  proxySettings: ProxySettings = 'none'
+): Promise<GeoInfo> {
+  const myFetch = IS_IN_BROWSER ? fetch : (await import('node-fetch')).default;
+  const proxyAgent =
+    IS_IN_BROWSER || proxySettings === 'none'
+      ? false
+      : await (async () =>
+          proxySettings === 'system'
+            ? new (
+                await import('proxy-agent')
+              ).ProxyAgent()
+            : proxySettings)();
+  const result = (await (
+    await myFetch(
+      `http://ip-api.com/json/${ip}?fields=status,message,continent,continentCode,country,countryCode,region,\
+      regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query`,
+      {
+        agent: proxyAgent,
+      }
+    )
+  )?.json()) as GeoInfoResponse;
+  return result.status === 'failed'
+    ? (() => {
+        throw {status: result.status, message: result.message};
+      })()
+    : getGeoInfoFromResponse(result);
+}
---
+++ typing.ts
@@ -0,0 +1,38 @@
+import type { ProxyAgent } from 'proxy-agent';
+
+export interface GeoInfo {
+  query: string;
+  continent: string;
+  continentCode: string;
+  region: string;
+  regionName: string;
+  city: string;
+  district: string;
+  zip: string;
+  lat: number;
+  lon: number;
+  timezone: string;
+  offset: number;
+  currency: string;
+  isp: string;
+  org: string;
+  as: string;
+  asname: string;
+  reverse: string;
+  mobile: boolean;
+  proxy: boolean;
+  hosting: boolean;
+}
+
+export interface GeoInfoSuccess extends GeoInfo {
+  status: 'success';
+}
+
+export interface GeoInfoFailed {
+  status: 'failed',
+  message: 'private range' | 'reserved range' | 'invalid query';
+}
+
+export type GeoInfoResponse = GeoInfoSuccess | GeoInfoFailed;
+
+export type ProxySettings = 'system' | 'none' | ProxyAgent;
